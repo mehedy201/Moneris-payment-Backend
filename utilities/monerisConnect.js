592695a249451b39ad56d957a98a9c3a7b306776
mehedy201 <mehedy643@gmail.com>
2026-04-13 17:41:37 +0600

    remove unused code from coneris commenct file

--- utilities/monerisConnect.js
+++ utilities/monerisConnect.js
@@ -1,32 +1,3 @@
-// const braintree = require("braintree");
-
-// let gateway = null;
-
-// module.exports = {
-//   connectToBraintree: function () {
-//     try {
-//       gateway = new braintree.BraintreeGateway({
-//         environment: braintree.Environment.Production,
-//         merchantId: process.env.BT_MERCHANT_ID,
-//         publicKey: process.env.BT_PUBLIC_KEY,
-//         privateKey: process.env.BT_PRIVATE_KEY,
-//       });
-//       console.log("✅ Successfully connected to Braintree.");
-//     } catch (err) {
-//       console.error("❌ Error connecting to Braintree:", err);
-//       throw err;
-//     }
-//   },
-
-//   braintreeGateway: function () {
-//     if (!gateway) {
-//       throw new Error("Braintree not initialized. Call connectToBraintree first.");
-//     }
-//     return gateway;
-//   },
-// };
-
-
 const axios = require("axios");
 
 let monerisConfig = null;
